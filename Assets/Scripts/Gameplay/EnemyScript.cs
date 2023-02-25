f34878fd02776b24c2f194839f7c68a76a5ec0a8
KonlawatPach <pach.wongsrithong@Gmail.com>
2023-02-25 10:58:40 +0700

    add sound & hp bar

--- Assets/Scripts/Gameplay/EnemyScript.cs
+++ Assets/Scripts/Gameplay/EnemyScript.cs
@@ -10,11 +10,20 @@ public class EnemyScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public TextMeshProUGUI BossHPText;
+    public Image healthBar;
     public XRState xrstatus;
-    public int BossHP = 100;
+    public AudioClip punchSound;
+    public AudioClip slapSound;
+    private AudioSource playerAudio;
+
+    public string bossname = "";
+    public float maxBossHP = 100f;
+    public float BossHP = 100f;
 
     void Start()
     {
+        BossHPText.text = bossname;
+        playerAudio = GetComponent<AudioSource>();
         xrstatus = GameObject.Find("XR Rig").GetComponent<XRState>();
     }
 
@@ -26,16 +35,53 @@ public class EnemyScript : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("LeftHand") && xrstatus.isLeftGripActive)
+        if ((other.CompareTag("LeftHand") && xrstatus.isLeftGripActive) || (other.CompareTag("RightHand") && xrstatus.isRightGripActive))
         {
-            BossHP -= Random.Range(4, 6);
-            BossHPText.text = "BOSS HP: " + BossHP.ToString();
+            playerAudio.PlayOneShot(punchSound);
+            BossHP -= Random.Range(7, 10);
+
+            BossHPText.text = bossname;
+            healthBar.fillAmount = BossHP / maxBossHP;
         }
-        else if (other.CompareTag("RightHand") && xrstatus.isRightGripActive)
+        else if ((other.CompareTag("LeftHand") && !xrstatus.isLeftGripActive) || (other.CompareTag("RightHand") && !xrstatus.isRightGripActive))
         {
-            BossHP -= Random.Range(4, 6);
-            BossHPText.text = "BOSS HP: " + BossHP.ToString();
+            playerAudio.PlayOneShot(slapSound);
+            BossHP -= Random.Range(1, 2);
+
+            BossHPText.text = bossname;
+            healthBar.fillAmount = BossHP / maxBossHP;
         }
     }
 
+
+
+
+    ///////////////////////START FUNCTION//////////////////////////
+    public TeleportationAnchor anchor = null;
+    public TeleportationProvider provider = null;
+
+    private void Awake()
+    {
+        if (anchor && provider)
+        {
+            TeleportRequest request = CreateRequest();
+            provider.QueueTeleportRequest(request);
+        }
+    }
+
+    private TeleportRequest CreateRequest()
+    {
+        Transform anchorTransform = anchor.teleportAnchorTransform;
+
+        TeleportRequest request = new TeleportRequest()
+        {
+            requestTime = Time.time,
+            matchOrientation = anchor.matchOrientation,
+
+            destinationPosition = anchorTransform.position,
+            destinationRotation = anchorTransform.rotation
+        };
+
+        return request;
+    }
 }
